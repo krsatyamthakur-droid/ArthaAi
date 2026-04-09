3115fdb1b4a7422157ef7631c2106dee514087f4
satyam kumar <krsatyamthakur@gamil.com>
2026-04-10 04:46:18 +0530

    Fix backend path resolution for Netlify Functions

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 const serverless = require('serverless-http');
-const app = require('./index');
+const app = require('../../backend/index');
 
 // Wrap the Express app and export it as a Netlify handler
 module.exports.handler = serverless(app);
