b9f523a45af7647923487ea544071ff0229e72ef
satyam kumar <krsatyamthakur@gamil.com>
2026-04-10 05:33:58 +0530

    Revert to Vercel hosting setup and cleanup Netlify files

--- netlify/functions/api.js
+++
@@ -1,5 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../backend/index');
-
-// Wrap the Express app and export it as a Netlify handler
-module.exports.handler = serverless(app);
